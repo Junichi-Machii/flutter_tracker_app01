687c0f3efb914b235387636fb5bacf71796c22b8
Junichi-Machii <ronmachii@gmail.com>
2023-07-13 11:03:58 +0900

    カードwidgetのフォルダ、ファイル作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:tracker_app/expenses.dart';
+import 'package:tracker_app/widgets/expenses.dart';
 void main(List<String> args) {
   runApp( 
    const MaterialApp(
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tracker_app/models/expense.dart';
+import 'package:tracker_app/widgets/expenses_list/expenses_item.dart';
 
 class ExpensesList extends StatelessWidget {
   const ExpensesList({super.key, required this.expenses});
@@ -10,7 +11,7 @@ class ExpensesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => Text(expenses[index].title),
-    );
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),);
+    
   }
 }
